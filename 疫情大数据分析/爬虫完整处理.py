0c0dcc2e9d099d2d16e3ea2ae20c41db155b0c21
FurutariRin <59307955+FurutariRin@users.noreply.github.com>
2021-08-24 10:57:36 +0800

    对应的代码修改

--- 疫情大数据分析/爬虫完整处理.py
+++ 疫情大数据分析/爬虫完整处理.py
@@ -301,3 +301,26 @@ for i in tqdm(range(0, 34), desc="写入省份最新数据", unit="个省"):
         highDangerCount, midDangerCount, suspectedCount, suspectedCountIncr
     ])
 f.close()
+
+import pandas as pd
+import csv
+from tqdm import tqdm
+
+filePath = "疫情大数据分析\\爬虫数据\\全国死亡率与治愈率.csv"
+f = open(filePath, "w", encoding="utf-8", newline="")
+csv_writer = csv.writer(f)
+csv_writer.writerow(["日期", "确诊总数", "治愈总数", "死亡人数",
+                    "感染死亡率", "感染治愈率", "住院死亡率", "住院治愈率"])
+UseData = pd.read_csv("疫情大数据分析\\爬虫数据\\全国数据.csv")
+for i in tqdm(range(len(UseData)), desc="计算死亡率与治愈率", unit="Day"):
+    Date = UseData.iloc[i]["日期"]
+    confirmedCount = UseData.iloc[i]["确诊总数"]
+    curedCount = UseData.iloc[i]["治愈总数"]
+    deadCount = UseData.iloc[i]["死亡人数"]
+    CDP = float(deadCount/confirmedCount)
+    CCP = float(curedCount/confirmedCount)
+    HDP = float(deadCount/(deadCount+curedCount))
+    HCP = float(curedCount/(deadCount+curedCount))
+    csv_writer.writerow([Date, confirmedCount, curedCount,
+                        deadCount, CDP, CCP, HDP, HCP])
+f.close()
