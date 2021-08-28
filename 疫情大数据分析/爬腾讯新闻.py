c80537b9af7d1f2549774322bb58be9aacf5628c
FurutariRin <59307955+FurutariRin@users.noreply.github.com>
2021-08-28 16:16:34 +0800

    提取量修改

--- 疫情大数据分析/爬腾讯新闻.py
+++ 疫情大数据分析/爬腾讯新闻.py
@@ -3,19 +3,19 @@ import requests
 from tqdm.std import tqdm
 
 # 链接offset为新闻偏移量，limit为显示数量
-url = "https://i.news.qq.com/trpc.qqnews_web.kv_srv.kv_srv_http_proxy/list?sub_srv_id=antip&srv_id=pc&offset=0&limit=20&strategy=1&ext={%22pool%22:[%22top%22],%22is_filter%22:10,%22check_type%22:true}"
+url = "https://i.news.qq.com/trpc.qqnews_web.kv_srv.kv_srv_http_proxy/list?sub_srv_id=antip&srv_id=pc&offset=0&limit=100&strategy=1&ext={%22pool%22:[%22top%22],%22is_filter%22:10,%22check_type%22:true}"
 head = {
     "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
 }
 req = requests.get(url).json()
 filePath = "疫情大数据分析\\爬虫数据\\新闻数据\\每日新闻.csv"
-f = open(filePath, "w", encoding="utf-8")
+f = open(filePath, "w", encoding="utf-8", newline="")
 csv_writer = csv.writer(f)
 csv_writer.writerow(["新闻标题", "URL", "IMGURL", "作者", "发布时间"])
 # print(len(req["data"]["list"]))
 # print(req["data"]["list"][0]["url"])
-for i in tqdm(range(len(req["data"]["list"])),desc="获取新闻数据",unit="条新闻"):
+for i in tqdm(range(len(req["data"]["list"])), desc="获取新闻数据", unit="条新闻"):
     csv_writer.writerow([
         req["data"]["list"][i]["title"], req["data"]["list"][i]["url"],
         req["data"]["list"][i]["img"], req["data"]["list"][i]["media_name"],
